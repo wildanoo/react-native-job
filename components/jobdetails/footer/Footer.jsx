b25dc933ea9e68bc5fae9fff30a2aa2a020c3106
Wildan Aryo Bimantoro <wildan.bimantoro_gue@GE-072142.HOSDG>
2023-06-25 10:06:12 +0700

    react apps job

---
+++ components/jobdetails/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+
+import styles from "./footer.style";
+import { icons } from "../../../constants";
+
+const Footer = ({ url }) => {
+   return (
+      <View style={styles.container}>
+         <TouchableOpacity style={styles.likeBtn}>
+            <Image source={icons.heartOutline} resizeMode="contain" style={styles.likeBtnImage} />
+         </TouchableOpacity>
+         <TouchableOpacity style={styles.applyBtn}>
+            <Text style={styles.applyBtnText} onPress={() => Linking.openURL(url)}>
+               Apply for job
+            </Text>
+         </TouchableOpacity>
+      </View>
+   );
+};
+
+export default Footer;
